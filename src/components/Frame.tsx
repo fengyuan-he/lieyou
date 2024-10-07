b3339ce1e4406c69fea3fff61271bfc08a2af858
gxz <cnmmail163@163.com>
2024-10-08 00:10:27 +0800

    拜拜

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,10 +1,8 @@
 import {Button} from "@/components/ui/button";
-import {Home, MessagesSquare} from "lucide-react";
+import {Home} from "lucide-react";
 import {useRouter} from "next/navigation";
 import {ReactNode} from "react";
 import manifest from "@/../public/manifest.json";
-import {AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent,
-    AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
 
 export default function Frame({title, header, actions, children}: {
     title?: string
@@ -27,41 +25,6 @@ export default function Frame({title, header, actions, children}: {
                     >
                         <Home className="w-4 h-4"/>
                     </Button>
-                    <AlertDialog>
-                        <AlertDialogTrigger asChild>
-                            <Button
-                                variant="outline"
-                                size="icon"
-                            >
-                                <MessagesSquare className="w-4 h-4"/>
-                            </Button>
-                        </AlertDialogTrigger>
-                        <AlertDialogContent>
-                            <div>
-                                <AlertDialogHeader className="max-h-[calc(100dvh-180px)] overflow-auto w-1/3 float-left">
-                                    <AlertDialogTitle>报名</AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        lala
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogHeader className="max-h-[calc(100dvh-180px)] overflow-auto w-1/3 float-left">
-                                    <AlertDialogTitle>操作</AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        lala
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogHeader className="max-h-[calc(100dvh-180px)] overflow-auto w-1/3 float-left">
-                                    <AlertDialogTitle>推演</AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        lala
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                            </div>
-                            <AlertDialogFooter>
-                                <AlertDialogCancel>关闭</AlertDialogCancel>
-                            </AlertDialogFooter>
-                        </AlertDialogContent>
-                    </AlertDialog>
                 </div>
             </div>
             {children}
